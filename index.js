bf870f06a792ea4ddf42ce70cb104b4d9ee5043f
Josh Rime <joshrime1990@icloud.com>
2024-02-27 21:47:16 -0600

    foreach loop

---
+++ index.js
@@ -0,0 +1,9 @@
+const clientName = window.prompt("Welcome to Kolors! Please enter your company's name: ")
+const palletebtn = document.getElementById('#palettebtn')
+//const colorBars = document.getElementsByTagName('div')
+const colorBars = document.querySelectorAll('.rectangle')
+const changePalette = () => {
+    colorBars.forEach((rectangleElement, rectangleIndex) => {
+        
+    })
+}
